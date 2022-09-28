f7057bd628d197535d9af0cf9d4174cd42074625
Datorium <hello@datorium.eu>
2022-09-28 14:17:47 +0300

    Game resuls now are stored in a list

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import random
 turns = ['rock', 'paper', 'scissors']
 human_turns = []
 computer_turns = []
+win_results = []
 
 while(True):
     human_turn = input("Enter your turn, or type exit: ")
@@ -17,15 +18,21 @@ while(True):
     print(f'Human:{human_turn} vs. Computer:{computer_turn}')
     if human_turn == computer_turn:
         print('Draw!')
+        win_results.append('Draw!')
     elif human_turn == 'rock' and computer_turn == 'scissors':
         print('Human wins!')
+        win_results.append('Human wins!')
     elif human_turn == 'paper' and computer_turn == 'rock':
         print('Human wins!')
+        win_results.append('Human wins!')
     elif human_turn == 'scissors' and computer_turn == 'paper':
         print('Human wins!')
+        win_results.append('Human wins!')
     else:
         print('Computer wins!')
+        win_results.append('Computer wins!')
 
 print(f'You have played {len(human_turns)} times')
 print(human_turns)
 print(computer_turns)
+print(win_results)
